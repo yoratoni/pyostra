500a5ec1e4209863aceadbf5e891291d12d9d8a3
Bibollet Adrien <71235233+yoratoni@users.noreply.github.com>
2022-03-05 14:12:06 +0100

    Update

---
+++ README.md
@@ -0,0 +1,1 @@
+Link: https://pypi.org/project/pyostra/
--- setup.py
+++ setup.py
@@ -4,17 +4,18 @@ from setuptools import setup, find_packages
 __description__ = 'THe internal Python package used for the Ostra projects such as the advanced NFTs generator.'
 __long_description__ = open('README.md').read() + '\n\n' + open('CHANGELOG.md').read()
 __classifiers__ = [
-    'Development Status :: 1 - Planning Copy',
+    'Development Status :: 5 - Production/Stable',
     'Programming Language :: Python :: 3',
-    'License :: OSI Approved :: MIT',
-    'Intended Audience :: Ostra environment',
-    'Operating System :: Microsoft :: Windows'
+    'License :: OSI Approved :: MIT License',
+    'Intended Audience :: Developers',
+    'Operating System :: Microsoft :: Windows',
+    'Topic :: Utilities'
 ]
 
 
 setup(
     name='pyostra',
-    version='0.0.1',
+    version='0.1.1',
     author='Yoratoni',
     author_email='twingearas@gmail.com',
     license='MIT',
@@ -24,7 +25,7 @@ setup(
     long_description=__long_description__,
 
     packages=find_packages(),
-    install_requires=['inspect', 'colorama', 'typing', 'time'],
+    install_requires=['colorama'],
     
     keywords='ostra',
     classifiers=__classifiers__
